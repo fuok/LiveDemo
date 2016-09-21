15e901137ff5b0e2cf15a91232a8077245d379e3
fuok <liuyang@Browns-Mac.local>
2016-09-21 17:37:10 +0800

    create data

---
+++ Assets/_scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using GameData;
+
+namespace MyNamespace
+{
+	
+	public class GameController : MonoBehaviour
+	{
+
+		private IEnumerator ie;
+
+		void Start ()
+		{
+			InitPara ();
+		}
+
+		void Update ()
+		{
+			
+			if (Input.GetMouseButtonDown (0)) {
+				if (ie.MoveNext ()) {
+					Paragraph para = ie.Current as Paragraph;
+					print (para.content);
+				} else {
+					print ("End!");
+				}
+			}
+			
+		}
+
+		
+		private void InitPara ()
+		{
+			TextAsset paraAsset = Resources.Load<TextAsset> ("paragraph/para_1");
+			ParagraphData mData = JsonConvert.DeserializeObject<ParagraphData> (paraAsset.text);
+			Resources.UnloadUnusedAssets ();
+			
+			if (true) {
+				print (mData.paragraphList.Count);
+				ie = mData.paragraphList.GetEnumerator ();
+			}
+		}
+	}
+}
+
---
+++ Assets/_scripts/ParagraphData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace GameData
+{
+	public class ParagraphData
+	{
+		public List<Paragraph> paragraphList = new List<Paragraph> ();
+	}
+
+	public class Paragraph
+	{
+		public int id;
+		public string content;
+	}
+}
+
