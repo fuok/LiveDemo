ba25775adfd14f60d51110125acbc8024be4d628
fuok <liuyang@Browns-Mac.local>
2016-10-05 12:00:36 +0800

    add db

--- Assets/_scripts/ParaManager.cs
+++ Assets/_scripts/ParaManager.cs
@@ -7,25 +7,23 @@ using System.IO;
 //通过DB操作，保存和读取Paragraph，只包含CRUD的CR
 public class ParaManager : MonoBehaviour
 {
+	private const string dbName = "live db.db";
 	private const string tableName = "paragraph";
 	private DbAccess db;
 
-	// Use this for initialization
 	void Start ()
 	{
-		db = new DbAccess ("data source=paragraph.db");//数据库名//("Server=127.0.0.1;UserId=root;Password=;Database=li")
-
+		//读取数据库
+		db = new DbAccess ("data source=" + dbName);//数据库名//("Server=127.0.0.1;UserId=root;Password=;Database=li")
 		//创建数据库表，与字段
 		db.CreateTable (tableName, new string[]{ "id", "content" }, new string[] {
 			"text",
 			"text"
 		}, false);
-
-
+		//初始化数据
 		StartCoroutine (InitPara ());
 	}
-	
-	// Update is called once per frame
+
 	void Update ()
 	{
 	
@@ -43,8 +41,7 @@ public class ParaManager : MonoBehaviour
 		while (ie.MoveNext ()) {
 			Paragraph para = ie.Current as Paragraph;
 			print (para.content);
-
-			db.InsertInto (tableName, new string[]{ para.id, para.content });
+			db.InsertInto (tableName, new string[]{ "'" + para.id + "'", "'" + para.content + "'" });
 		}
 
 	}
