74d21c05bc97ef466a8738eea716fafd8c2911b2
fuok <123>
2016-11-09 23:14:32 +0800

    add android db

--- Assets/_scripts/Constants.cs
+++ Assets/_scripts/Constants.cs
@@ -9,11 +9,11 @@ public class Constants
 	public const string tableName = "paragraph";
 	//数据库版本号
 	public const int dataBaseVersion = 1;
-	//数据库地址(自行创建)
-	public const string dbPath = "./assets/" + dbName;//注意不要放在根目录下,Application.dataPath会读取assets下面的
-	//PC数据库地址(外部导入)
-//	public static string dbPathPc = "URI=file:" + Application.streamingAssetsPath + "/" + dbName;
-	//Android数据库地址(外部导入)
-//	public static string dbPathAndroid = "URI=file:" + Application.persistentDataPath + "/" + dbName;
+	//数据库地址(自行创建),//注意不是在根目录下,Application.dataPath会读取./assets/下面的
+	public static string dbPath = Application.dataPath + "/" + dbName;
+	//数据库地址(外部导入)
+	public static string dbPathPc = "URI=file:" + Application.streamingAssetsPath + "/" + dbName;
+	//Android数据库地址
+	public static string dbPathAndroid = Application.persistentDataPath + "/" + dbName;
 
 }
